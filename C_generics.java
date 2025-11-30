4b800e781d9f05d724c8c9621e451ecc31440c62
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-11-30 21:49:32 +0530

    Genetics work

--- C_generics.java
+++ C_generics.java
@@ -11,11 +11,18 @@ public class C_generics {
 		Itype<String> mak1 = new Maker1();
 		mak1.show("John");
 		
-		Itype<?> mak2 = new Maker2();
+//		Itype<T> mak2 = new Maker2();
 		
 		Itype<String> mak3 = new Converters();
 		mak3.show("Targaryen");
 	    System.out.println(mak3.dos("Lannisters"));
+	    
+	    Itype2<String,Integer> mak4 = new Conver();
+	    System.out.println(mak4.see(23));
+	    
+	    Itype2<Integer,String> mak5 = new Conver2();
+	    System.out.println(mak5.see("Jannifer Lawrence"));
+	   
 	}
 
 }
@@ -65,4 +72,24 @@ class Converters implements Itype<String>{
 	}
 }
 
+interface Itype2 <T,R>{
+	
+	 T see(R re);
+	
+}
+
+class Conver implements Itype2 <String,Integer>{
+	public String see(Integer a) {
+		if(a > 18) {
+			return "Vote Eligible";
+		}
+		return "try again";
+	}
+}
+
+class Conver2 implements Itype2 <Integer,String>{
+	public Integer see(String st) {
+		return st.length();
+	}
+}
 
