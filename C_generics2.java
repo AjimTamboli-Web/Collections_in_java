823aa6faeb3a7e44818fc606765ee2f1402a9e94
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-11-30 21:50:16 +0530

    generics operations

---
+++ C_generics2.java
@@ -0,0 +1,32 @@
+package collections;
+
+public class C_generics2 {
+
+	public static void main(String[] args) {
+		
+		Itypo t = new Cleverse();
+		t.see(23, 23);
+		t.see("Name" , "John");
+
+	}
+
+}
+
+interface Itypo<T>{
+	
+	void see(T t1,T t2);
+	
+}
+
+class Cleverse<T> implements Itypo<T>{
+	
+	public void see(T t1, T t2) {
+		if(t1 instanceof Integer && t2 instanceof Integer) {
+			 System.out.println((int)t1 + (int)t2);
+		}
+		else if(t1 instanceof String && t2 instanceof String) {
+			System.out.println("String Found: " + (String)t1 + (String)t2);
+		}
+	}
+	
+}
