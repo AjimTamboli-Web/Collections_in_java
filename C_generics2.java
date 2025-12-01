78d0058f8327c98c3de1586d04dcb5de01f6821c
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-12-01 15:00:41 +0530

    Generics adv

--- C_generics2.java
+++ C_generics2.java
@@ -4,12 +4,17 @@ public class C_generics2 {
 
 	public static void main(String[] args) {
 		
-		Itypo t = new Cleverse();
+		Itypo t = new Cleverse();  // raw type
 		t.see(23, 23);
 		t.see("Name" , "John");
-
+      
+		Itypo<Integer> inte = new Cleverse<Integer>();  // type safe 
+		inte.see(34, 34);
+		
+		Itypo<String> strin = new Cleverse<String>();
+		strin.see("Name: ", "John");
+		
 	}
-
 }
 
 interface Itypo<T>{
@@ -28,5 +33,4 @@ class Cleverse<T> implements Itypo<T>{
 			System.out.println("String Found: " + (String)t1 + (String)t2);
 		}
 	}
-	
 }
