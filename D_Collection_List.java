c43bbf92c48fd6fb7f36ef5c827972dfaf0ddd88
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-12-03 13:44:13 +0530

    List overview

--- D_Collection_List.java
+++ D_Collection_List.java
@@ -7,13 +7,19 @@ public class D_Collection_List {
 
 	public static void main(String[] args) {
 	
-		List list = new DD();      // generic
+		List list = new DD();      // generic raw type
           list.add(23);
           list.add("AJJ");
           list.add(new int[] {34,23,54});
           System.out.println(list);
           
-          
+       List<Integer> list2 = new DD<Integer>();    // type safe
+        list2.add(23);
+        list2.add(25);
+        System.out.println(list2);
+        
+       DD list3 = new DD();
+       list3.shows(list2);
 	}
 
 }
