48a824ce005f9a4b12f4256e41e4ee640acbda09
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-12-05 19:41:16 +0530

    Generic Operations

--- C_generics2.java
+++ C_generics2.java
@@ -13,6 +13,11 @@ public class C_generics2 {
 		
 		Itypo<String> strin = new Cleverse<String>();
 		strin.see("Name: ", "John");
+	
+		
+		CharStr converters = new CharStr();
+		System.out.println(converters.charge("John","Walker"));
+		
 		
 	}
 }
@@ -21,6 +26,9 @@ interface Itypo<T>{
 	
 	void see(T t1,T t2);
 	
+}
+interface Conv<T,R>{
+	R charge(T t1,T t2);
 }
 
 class Cleverse<T> implements Itypo<T>{
@@ -33,4 +41,29 @@ class Cleverse<T> implements Itypo<T>{
 			System.out.println("String Found: " + (String)t1 + (String)t2);
 		}
 	}
+}
+
+
+class CharStr implements Conv<String,Character[]>{
+
+	@Override
+	public Character[] charge(String t1, String t2) {
+		
+		char[] char1 = t1.toCharArray();
+		char[] char2 = t2.toCharArray();
+		
+		int i = char1.length+char2.length;
+		
+		Character[] chars = new Character[i];
+		
+		for(int j = 0; j < char1.length; j++) {
+			chars[j] = Character.valueOf(char1[j]);
+		}
+		
+		return chars;
+	}
+	
+	
+	
+	
 }
