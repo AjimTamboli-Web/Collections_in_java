c2ddba48b4e67ef0d4930c6c5e44bd7ef2523419
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-12-05 19:42:21 +0530

    Overview of Colletion

--- D_Collection_List.java
+++ D_Collection_List.java
@@ -1,33 +1,57 @@
 package collections;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class D_Collection_List {
 
 	public static void main(String[] args) {
-	
-		List list = new DD();      // generic raw type
-          list.add(23);
-          list.add("AJJ");
-          list.add(new int[] {34,23,54});
-          System.out.println(list);
-          
-       List<Integer> list2 = new DD<Integer>();    // type safe
-        list2.add(23);
-        list2.add(25);
-        System.out.println(list2);
-        
-       DD list3 = new DD();
-       list3.shows(list2);
+
+/*
+ * ⏺️ Collections => Collection in Java is a framework that provides classes and interfaces to store and manipulate groups of objects.
+ * 				    	It is found in:	 👉 java.util package
+ * 		      The Java Collections class is used with the static methods that operate on the collections or return the collection. 
+ * ⚠️ Why Collection Framework?
+		Before collections, we used arrays, but arrays have drawbacks:  ❌ Fixed size  
+		 		Iterable												❌ Only same type of elements
+		            ↑													❌ No built-in methods (sort, search, add/remove dynamically)
+		   		Collection													Collections solve all this.     
+		   		    ├── List  (Ordered, Allows Duplicates)
+		   		    ├── Set   (No Duplicates)
+		   		    └── Queue  (FIFO: First In First Out)                                                      
+ *		
+ */
+		
+		List<Integer> al = new ArrayList<Integer>();  // create a arrayList to store elements.
+		
+		al.add(12);  al.add(54);  al.add(98);  al.add(76);  al.add(12);  al.add(67);  al.add(56);   al.add(98);
+		
+		System.out.println(al);
+		
+		List<String> ll = new LinkedList<String>();	  // create a LinkedList to store elements.
+		
+		ll.add("John"); ll.add("Dany"); ll.add("Robb"); ll.add("Jamie"); ll.add("Tyrion");  ll.add("Arya"); ll.add("Cersey");
+		
+		System.out.println(ll);
+// *************************************************************************************************************************
+		
+		Set<Integer> hs = new HashSet<Integer>();
+		
+		hs.add(12); hs.add(56); hs.add(12); hs.add(65); hs.add(54); hs.add(34); hs.add(78); hs.add(98); hs.add(34);
+		
+		System.out.println(hs);
+		
+		Set<String> ts = new TreeSet<String>();
+		
+		ts.add("Aeron");  ts.add("Daemon");  ts.add("Stefan");  ts.add("Elena");  ts.add("Elija");  ts.add("Aeron");    
+		
+		System.out.println(ts);
+		
 	}
 
 }
 
-class DD<T> extends ArrayList<T> {
-	
-	public void shows(T i) {
-		System.out.println(i);
-	}
-	
-}
