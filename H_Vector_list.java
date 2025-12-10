c864f66a390e224fa92c74322e5a21671ebd0f99
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-12-10 16:56:14 +0530

    Vector overview

--- H_Vector_list.java
+++ H_Vector_list.java
@@ -1,10 +1,53 @@
 package collections;
 
+import java.util.*;
+
 public class H_Vector_list {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+/*
+ * Vector is a legacy class in Java (introduced in JDK 1.0).  It is similar to ArrayList but:
+	 ✔ Thread-safe (synchronized)   ✔ Slower than ArrayList    ✔ Grows automatically (dynamic array)
+        Vector is part of java.util package.		
+   ✔ 1. Dynamic Array  Size grows automatically.  Default capacity = 10
+         When full, its capacity doubles (capacity × 2).
+   ✔ 2. Synchronized (Thread-safe)   All methods are synchronized.
+         That means only one thread can access a Vector method at a time.
+		 Result:  Safe for multithreading , But slower performance      
+   ✔ 3. Allows duplicates + NULL + maintains insertion order , Same as ArrayList:
+		 ✔ Duplicate values allowed    ✔ NULL allowed    ✔ Order is maintained   
+   ✔ 4. Legacy class -> Before Java Collections Framework existed.
+         Still used only when: ➡ Multithreading + dynamic array required	  
+       
+ *   Java Vector contains many legacy methods that are not the part of a collections framework.
+ *   
+ */
+		
+		Vector<String> v = new Vector<String>();
+		
+		v.add("A");
+		v.add("B");
+		v.add("C");
+		v.add(null);
+		v.add("D");
+		v.add("E");
+		v.add(null); // accept null value
+		v.add("H");
+		v.add("R");
+		v.add("A");  // duplicate allowed
+		
+		System.out.println(v.capacity());  // default 10  newCapacity = oldCapacity * 2 
+		v.add("Y"); // add 11 number
+		
+		System.out.println(v.capacity());  // now capacity is 20
+		
+		System.out.println(v);
+		
+		Vector<String> v2 = new Vector<String>(4);
+		
+		System.out.println(v2.capacity());  // capacity is 4
+		
 	}
 
 }
