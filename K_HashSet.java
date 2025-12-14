1a4eab39c9c294474c1bc4fc9ead6803626f2095
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-12-14 22:01:19 +0530

    Hash set op

--- K_HashSet.java
+++ K_HashSet.java
@@ -7,13 +7,16 @@ public class K_HashSet {
 	public static void main(String[] args) {
 		
 /*
+ *  Default capacity is 16
+ *  and load factor is 0.75
+ *  there is no guarantee of insertion order
  * 	
  */
 		
 		Hashing ha = new Hashing(1,"Home",234.54);
 		Hashing ha1 = new Hashing(2,"Work",234.56);
 		Hashing ha2 = new Hashing(1,"Home",234.54);
-		
+	
 		HashSet<Hashing> hashh = new HashSet<Hashing>();
 		
 		hashh.add(ha);
@@ -23,7 +26,12 @@ public class K_HashSet {
 		
 		System.out.println(ha.equals(ha2));
 		
-
+        System.out.println("************************************************");
+   
+		HashSet<Integer> haha = new HashSet<>();
+		haha.add(12);
+		System.out.println(haha.hashCode());
+		System.out.println(Integer.hashCode(23));
 	}
 
 }
