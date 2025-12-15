2e02f104b829ce37eb123195ed46f7219a31719b
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-12-15 22:04:41 +0530

    Hash Set Intro

--- K_HashSet.java
+++ K_HashSet.java
@@ -7,10 +7,24 @@ public class K_HashSet {
 	public static void main(String[] args) {
 		
 /*
- *  Default capacity is 16
- *  and load factor is 0.75
- *  there is no guarantee of insertion order
- * 	
+ * HashSet in Java implements the Set interface of the Collections Framework. 
+    It is used to store the unique elements, and it doesn't maintain any specific order of elements.
+ *  Default capacity is 16  and load factor is 0.75 there is no guarantee of insertion order.
+ * 	HashSet does not allow duplicate elements.
+ *  Uses HashMap internally which is an implementation of hash table data structure.
+ *  Also implements Serializable and Cloneable interfaces.
+ *  HashSet is not thread-safe. To make it thread-safe, synchronization is needed externally.
+   
+ *  💡 Why HashSet is Fast?  =>> Because it uses a HashMap internally, which is based on a hash table.
+ *  ⭐ Why HashSet Ignored Duplicates?  =>> Because it checks:
+															✔️ hashCode()
+															✔️ equals()
+		If both match → duplicate → reject.  That’s why custom objects require overriding both methods.
+ * 
+ * ⭐ When to use HashSet?  =>> Use HashSet when:   ✔️  Fast search needed    ✔️  No duplicates allowed
+												  ✔️  Order doesn’t matter   ✔️  Want unique list	
+ * It inherits the AbstractSet class and implements Set interface.
+ * 													
  */
 		
 		Hashing ha = new Hashing(1,"Home",234.54);
