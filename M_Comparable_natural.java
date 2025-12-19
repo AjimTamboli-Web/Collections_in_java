e1b85290b2f97a647f1faef5f3bc4a6c5e71ac86
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-12-19 08:13:58 +0530

    Comparable / Natural Sorting

--- M_Comparable_natural.java
+++ M_Comparable_natural.java
@@ -8,7 +8,7 @@ public class M_Comparable_natural {
 
 /* 
  * ⏺️ Elements are sorted using:
-			Comparable (natural ordering), or
+			Comparable (natural ordering)- ascending and descending order, or
 			Comparator (custom ordering)
    ⏺️ Elements are placed based on compareTo() / compare(), NOT hashCode()
     
@@ -18,8 +18,21 @@ public class M_Comparable_natural {
   				Negative value  → current object < given object		 10.compareTo(20) → -1	
 				Zero            → current object == given object		 20.compareTo(20) → 0		
 				Positive value  → current object > given object		 30.compareTo(20) → +1	
- * 		
+ * comparable implements in same class only and it present since 1.2 version
+ * That have only one method in Comparable<T> interface.(public int compareTo(T o);) but it is not functional 
+   interface because functional interface added in 1.8 version.
+ * Note: this class has a natural ordering that is inconsistent with equals.
+ * @param   o the object to be compared.
+ * @return  a negative integer, zero, or a positive integer as this object
+      is less than, equal to, or greater than the specified object.
+ * It is modifying the main source code for sorting.    
  */
+		String st = "John";
+		String st2 = "Johnny";
+		System.out.println("Compare: " + st2.compareTo(st));
+		System.out.println("IgnoreCase: " + st2.compareToIgnoreCase(st));
+		
+		
 		Sort_objects one1 = new Sort_objects(12,"Teleporter");
 		Sort_objects two2 = new Sort_objects(43,"Flask");
 		Sort_objects three3 = new Sort_objects(2,"Vispa");
